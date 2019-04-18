e5ab43cb8c18943346f2637ebf9428b022507c89
Tung <tung@test.com>
2019-04-18 20:41:58 +0700

    completed blog url slug

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,10 +8,14 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.Slug = require('slug');
+Slug.defaults.mode ='rfc3986';
 
 import Buefy from 'buefy';
 
 Vue.use(Buefy);
+
+Vue.component('slug-widget', require('./components/slugWidget.vue').default);
 
 require('./manage');
 
---
+++ resources/views/manage/posts/create.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.manage')
+
+@section('content')
+<div class="flex-container">
+	<div class="columns m-t-10">
+		<div class="column">
+			<h1 class="title">This is the posts.create page</h1>
+		</div>
+		<div class="column">
+			{{--  <a href="{{route('posts.create')}}" class="button is-primary is-pulled-right"><i class="fa fa-user-plus m-r-10"></i> Create New Post</a> --}}
+		</div>
+	</div>
+	<hr class="m-t-0">
+
+	<form action="{{route('posts.store')}}" method="post">
+		{{ csrf_field() }}
+		<div class="columns">
+			<div class="column is-three-quarters-desktop is-three-quarters-tablet">
+				<b-field>
+					<b-input type="text" placeholder="Post Title" size="is-large" v-model="title">
+					</b-input>
+				</b-field>
+				
+				<slug-widget url="{{url('/')}}" subdirectory="blog" :title="title" @slug-changed="updateSlug"></slug-widget>
+				<input type="hidden" name="slug" v-model="slug">
+
+				<b-field class="m-t-30">
+					<b-input type="textarea" placeholder="Compose your masterpiece.." rows="10">
+					</b-input>
+				</b-field>
+			</div> <!-- end of .column.is-three-quarters -->
+
+			<div class="column is-one-quarter-desktop is-narrow-tablet">
+				<div class="card card-widget">
+					<div class="author-widget widget-area">
+						<div class="selected-author">
+							<img src="https://placehold.it/50x50">
+							<h4>Huu Tung</h4>
+							<p class="subtitle">(tung)</p>
+						</div>
+					</div>
+					<div class="post-status-widget widget-area">
+						<div class="status">
+							<div class="status-icon">
+								<b-icon icon="assignment"></b-icon>
+							</div>
+							<div class="status-details">
+								<h4>Draft Saved</h4>
+								<p>A Few Minutes Ago (saved)</p>
+							</div>
+						</div>
+					</div>
+					<div class="publish-buttons-widget widget-area">
+						<div class="secondary-action-button">
+							<button class="button is-info is-outlined is-fullwidth">Save Draft</button>
+						</div>
+						<div class="primary-action-button">
+							<button class="button is-primary is-fullwidth">Publish</button>
+						</div>
+					</div>
+				</div>
+			</div> <!-- end of .column.is-one-quarter -->
+		</div>
+	</form>
+
+</div> <!-- end of .flex-container -->
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+	window.addEventListener('load', function(){
+		var app = new Vue({
+			el: '#app',
+			data: {
+				title: '',
+				slug: ''
+			},
+			methods: {
+				updateSlug: function(val) {
+					this.slug = val;
+				}
+			}
+		});
+	});
+</script>
+@endsection
